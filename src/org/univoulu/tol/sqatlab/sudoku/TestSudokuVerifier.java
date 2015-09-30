c3941d6936ebdf5bb2fb3f3350b0dba34df163ea
somename <someemail>
2015-09-30 15:07:29 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -10,8 +10,9 @@ public class TestSudokuVerifier {
 	
 	@Test
 	public void test() {
-		// GitHub testing
-		fail("Not yet implemented");
+		
+		verifier.verifyPositiveDigits(String "417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		
 	}
 
 }
