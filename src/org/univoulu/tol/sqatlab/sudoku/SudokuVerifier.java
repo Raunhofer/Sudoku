a4647eeda402b11175f98d7dd745d6114ca6f199
somename <someemail>
2015-09-30 16:07:51 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -32,6 +32,7 @@ public class SudokuVerifier {
 		String[] array = candidateSolution.split("");
 		for (int x = 0; x < array.length; x++) {
 			for (int i = x+1; i < array.length; i++) {
+				System.out.println(array[i] + " " array[x]);
 				if (array[i] == array[x]) {
 					return false;
 				}
