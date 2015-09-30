49852af44cc1e3992afc072c6efdd844e0deb275
somename <someemail>
2015-09-30 15:08:32 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -13,7 +13,7 @@ public class TestSudokuVerifier {
 	
 	@Test
 	public void test() {
-		verifier.verifyPositiveDigits("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		verifier.verifyPositiveDigits(valid);
 		
 	}
 
