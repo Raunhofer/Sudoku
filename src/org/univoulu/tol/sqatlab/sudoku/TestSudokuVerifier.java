c2821065e891cf1f9ea9e961042ab52885262663
somename <someemail>
2015-09-30 16:00:55 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -36,7 +36,7 @@ public class TestSudokuVerifier {
 	@Test
 	public void testGlobalGridRows() {
 		SudokuVerifier verifier = new SudokuVerifier();
-		assertEquals(true, verifier.verifyGlobalGridRows("111"));
+		assertEquals(true, verifier.verifyGlobalGridRows("11234567891"));
 	}
 
 }
