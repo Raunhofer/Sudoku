7251cd0d0b2876eb8a39fa271cd9fac53604eebc
somename <someemail>
2015-10-03 18:10:05 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -46,9 +46,8 @@ public class SudokuVerifier {
 			// Number-at-time
 			//StringBuilder row = new StringBuilder();
 			int[] row = {0,0,0,0,0,0,0,0};
-			row[0] = 1;
-			System.out.println(row[0]);
-			
+
+			System.out.println(candidateArr[0]);
 		}
 		
 		/*
