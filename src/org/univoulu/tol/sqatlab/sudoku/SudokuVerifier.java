7ff038e402bcd0c8f4c81bf4c8041df27fe10ce2
somename <someemail>
2015-10-04 14:07:37 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -28,12 +28,11 @@ public class SudokuVerifier {
 		boolean verifyResult = true;
 		char[] charArray = candidateSolution.toCharArray();
 		String girdstr = "";
-		for (int i=0; i<=8; i++) {
-			
-		}
-		for (int x=0; x<=8; x++) {
-			for (int r=0; r<=2; r++) {
-				girdstr = girdstr + charArray[x+(r*9)];
+		for (int i=0; i<=2; i++) {
+			for (int x=0; x<=8; x++) {
+				for (int r=0; r<=2; r++) {
+					girdstr = girdstr + charArray[(x+(i*27))+(r*9)];
+				}
 			}
 		}
 		System.out.println(girdstr);
